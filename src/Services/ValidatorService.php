fcdcb0aa5ae59a00c8146ac7f6d4495a12eddcf0
John A. Marsh <github@marshmail.us>
2024-10-17 10:41:09 +0100

    Allow Validator to be run directly or after retrieving by ID

--- src/Services/ValidatorService.php
+++ src/Services/ValidatorService.php
@@ -17,12 +17,27 @@ class ValidatorService
     /**
      * Runs a {@link Validator} against the provided data.
      *
+     * @param Validator $validator The {@link Validator}
+     * @param array     $data      The data to validate
+     *
+     * @return bool true, if the data is valid
+     */
+    public function runValidator(Validator $validator, array $data): bool
+    {
+        $rules = $validator->rules()->get();
+
+        return $this->ruleService->validateRules($rules->all(), $data);
+    }
+
+    /**
+     * Runs a {@link Validator} against the provided data.
+     *
      * @param string $validator_id The {@link Validator::$validator_id} or {@link Validator::name}
      * @param array  $data         The data to validate
      *
      * @return bool true, if the data is valid
      */
-    public function runValidator(string $validator_id, array $data): bool
+    public function runValidatorById(string $validator_id, array $data): bool
     {
         $validator = Validator::where('active_status', ActiveStatus::ACTIVE)
             ->where(function ($query) use ($validator_id) {
@@ -31,12 +46,6 @@ class ValidatorService
             })
             ->first();
 
-        if (!$validator) {
-            return false;
-        }
-
-        $rules = $validator->rules()->get();
-
-        return $this->ruleService->validateRules($rules->all(), $data);
+        return $validator && $this->runValidator($validator, $data);
     }
 }
